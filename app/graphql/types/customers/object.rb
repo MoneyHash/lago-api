1557e4d755045b14dc4af53505c4cffcf7378662
Ahmed Shahwan <a.shahwan42@gmail.com>
2025-01-31 02:20:31 +0200

    better refactor Types::Customers::Object#provider_customer

--- app/graphql/types/customers/object.rb
+++ app/graphql/types/customers/object.rb
@@ -126,13 +126,18 @@ module Types
       end
 
       def provider_customer
-        case object&.payment_provider&.to_sym
-        when :stripe then object.stripe_customer
-        when :gocardless then object.gocardless_customer
-        when :cashfree then object.cashfree_customer
-        when :adyen then object.adyen_customer
-        when :moneyhash then object.moneyhash_customer
-        end
+        return unless object
+
+        provider_mapping = {
+          stripe: :stripe_customer,
+          gocardless: :gocardless_customer,
+          cashfree: :cashfree_customer,
+          adyen: :adyen_customer,
+          moneyhash: :moneyhash_customer
+        }
+
+        method = provider_mapping[object.payment_provider&.to_sym]
+        object.public_send(method) if method
       end
 
       def credit_notes_credits_available_count
