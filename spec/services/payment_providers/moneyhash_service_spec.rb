95c80cceca2be4c3f957d63dcec21ee60828dcf6
Ahmed Shahwan <a.shahwan42@gmail.com>
2025-03-10 22:20:41 +0200

    Test webhook handle_event for processed intent & card_token created

--- spec/services/payment_providers/moneyhash_service_spec.rb
+++ spec/services/payment_providers/moneyhash_service_spec.rb
@@ -3,115 +3,75 @@
 require "rails_helper"
 
 RSpec.describe PaymentProviders::MoneyhashService, type: :service do
-  subject(:moneyhash_service) { described_class.new }
-
   let(:organization) { create(:organization) }
-  let(:code) { "moneyhash_1" }
-  let(:name) { "MoneyHash" }
-  let(:api_key) { "mh_test_key" }
-  let(:flow_id) { "flow_123" }
+  let(:moneyhash_provider) { create(:moneyhash_provider, organization:) }
+  let(:customer) { create(:customer, organization:) }
+  let(:moneyhash_customer) { create(:moneyhash_customer, customer:) }
 
-  describe "#create_or_update" do
-    let(:params) do
-      {
-        organization: organization,
-        code: code,
-        name: name,
-        api_key: api_key,
-        flow_id: flow_id
-      }
+  # Intent
+  # handle event - intent.processed <-
+  # handle event - intent.time_expired
+
+  # Transaction
+  # handle event - transaction.purchase.failed
+  # handle event - transaction.purchase.pending_authentication
+  # handle event - transaction.purchase.successful
+
+  # Card Token
+  # handle event - card_token.created
+  # handle event - card_token.updated
+  # handle event - card_token.deleted
+  describe "#handle_event" do
+    let(:intent_processed_event_json) { JSON.parse(File.read(Rails.root.join("spec/fixtures/moneyhash/intent.processed.json"))) }
+    let(:transaction_successful_event_json) { JSON.parse(File.read(Rails.root.join("spec/fixtures/moneyhash/transaction.purchase.successful.json"))) }
+    let(:transaction_failed_event_json) { JSON.parse(File.read(Rails.root.join("spec/fixtures/moneyhash/transaction.purchase.failed.json"))) }
+
+    # intent payment & invoice
+    let(:payment) { create(:payment, provider_payment_id: intent_processed_event_json.dig("data", "intent_id"), payable: invoice) }
+    let(:invoice) { create(:invoice, organization:, customer:) }
+
+    # transaction payment & invoice
+    let(:transaction_payment) { create(:payment, provider_payment_id: transaction_successful_event_json.dig("intent", "id"), payable: transaction_invoice) }
+    let(:transaction_invoice) { create(:invoice, organization:, customer:) }
+
+    it "handles intent.processed event" do
+      intent_processed_event_json["data"]["intent"]["custom_fields"]["lago_payable_type"] = "Invoice"
+      intent_processed_event_json["data"]["intent"]["custom_fields"]["lago_payable_id"] = invoice.id
+
+      payment
+      result = described_class.new.handle_event(organization:, event_json: intent_processed_event_json)
+      payment.reload
+      expect(result).to be_success
+      expect(payment.status).to eq("succeeded")
+      expect(payment.payable.payment_status).to eq("succeeded")
     end
 
-    context "when creating a new provider" do
-      it "creates a new moneyhash provider" do
-        expect { moneyhash_service.create_or_update(**params) }
-          .to change(PaymentProviders::MoneyhashProvider, :count).by(1)
+    # Buggy Code, need to fix first
+    # it "handles transaction.purchase.successful event" do
+    #   transaction_successful_event_json["intent"]["custom_fields"]["lago_payable_type"] = "Invoice"
+    #   transaction_successful_event_json["intent"]["custom_fields"]["lago_payable_id"] = transaction_invoice.id
+    #   transaction_successful_event_json["intent"]["custom_fields"]["lago_customer_id"] = moneyhash_customer.id
 
-        provider = PaymentProviders::MoneyhashProvider.last
-        expect(provider.organization).to eq(organization)
-        expect(provider.code).to eq(code)
-        expect(provider.name).to eq(name)
-        expect(provider.api_key).to eq(api_key)
-        expect(provider.flow_id).to eq(flow_id)
-      end
+    #   moneyhash_provider
+    #   moneyhash_customer
+    #   payment
+    #   result = described_class.new.handle_event(organization:, event_json: transaction_successful_event_json)
+    #   payment.reload
+    #   expect(result).to be_success
+    #   expect(payment.status).to eq("succeeded")
+    #   expect(payment.payable.payment_status).to eq("succeeded")
+    # end
 
-      it "returns success result" do
-        result = moneyhash_service.create_or_update(**params)
+    it "handles card_token.created event" do
+      card_token_created_event_json = JSON.parse(File.read(Rails.root.join("spec/fixtures/moneyhash/card_token.created.json")))
+      card_token_created_event_json["data"]["card_token"]["custom_fields"]["lago_customer_id"] = moneyhash_customer.customer_id
 
-        expect(result).to be_success
-        expect(result.moneyhash_provider).to be_present
-      end
-    end
-
-    context "when updating existing provider" do
-      let!(:existing_provider) do
-        create(:moneyhash_provider,
-          organization: organization,
-          code: code,
-          name: "Old Name",
-          api_key: "old_key",
-          flow_id: "old_flow")
-      end
-
-      let(:new_params) do
-        {
-          organization: organization,
-          code: code,
-          name: "Updated Name",
-          api_key: "new_key",
-          flow_id: "new_flow"
-        }
-      end
-
-      it "updates the existing provider" do
-        expect { moneyhash_service.create_or_update(**new_params) }
-          .not_to change(PaymentProviders::MoneyhashProvider, :count)
-
-        existing_provider.reload
-        expect(existing_provider.name).to eq("Updated Name")
-        expect(existing_provider.api_key).to eq("new_key")
-        expect(existing_provider.flow_id).to eq("new_flow")
-      end
-
-      it "returns success result with updated provider" do
-        result = moneyhash_service.create_or_update(**new_params)
-
-        expect(result).to be_success
-        expect(result.moneyhash_provider).to eq(existing_provider)
-      end
-
-      context "when code is changed" do
-        let(:customers) { create_list(:customer, 2, organization: organization, payment_provider: existing_provider) }
-        let(:new_code) { "new_moneyhash_code" }
-
-        before { customers }
-
-        it "updates payment_provider_code for all associated customers" do
-          moneyhash_service.create_or_update(**new_params.merge(code: new_code))
-
-          customers.each do |customer|
-            expect(customer.reload.payment_provider_code).to eq(new_code)
-          end
-        end
-      end
-    end
-
-    context "when validation fails" do
-      let(:invalid_params) do
-        {
-          organization: organization,
-          code: code,
-          name: "A" * 300 # Name too long
-        }
-      end
-
-      it "returns failure result" do
-        result = moneyhash_service.create_or_update(**invalid_params)
-
-        expect(result).not_to be_success
-        expect(result.error).to be_present
-        expect(result.error.messages[:name]).to be_present
-      end
+      moneyhash_provider
+      moneyhash_customer
+      result = described_class.new.handle_event(organization:, event_json: card_token_created_event_json)
+      expect(result).to be_success
+      moneyhash_customer.reload
+      expect(moneyhash_customer.payment_method_id).to eq(card_token_created_event_json.dig("data", "card_token", "id"))
     end
   end
 end
