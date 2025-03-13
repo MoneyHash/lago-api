c373435a1131e3f30fb12b84b8fba98e598e73cf
Ahmed Shahwan <a.shahwan42@gmail.com>
2025-03-13 14:22:42 +0200

    Refine CreateService & its tests

--- app/services/payment_providers/moneyhash/payments/create_service.rb
+++ app/services/payment_providers/moneyhash/payments/create_service.rb
@@ -20,10 +20,11 @@ module PaymentProviders
           moneyhash_result = create_moneyhash_payment
 
           payment.provider_payment_id = moneyhash_result.dig("data", "id")
-          payment.status = moneyhash_result.dig("data", "status") || "pending"
+          payment.status = moneyhash_result.dig("data", "status") || moneyhash_result.dig("data", "active_transaction", "status") || "PENDING"
           payment.payable_payment_status = payment.payment_provider&.determine_payment_status(payment.status)
           payment.save!
 
+          result.payment = payment
           result
         end
 
